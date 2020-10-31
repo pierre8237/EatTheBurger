9cec38c1ab2b3cb3c88b3c612e3c9b3cac550ca6
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-31 08:33:18 -0400

    settingup jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,29 @@
 const mysql = require("mysql");
-const util = require("util");
+var connection;
+// const util = require("util");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mr.Wills123!!",
-  database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Mr.Wills123!!",
+//   database: "burgers_db"
+// });
 
-connection.connect(function (err) {
-  if (err) throw err;
-});
+// connection.connect(function (err) {
+//   if (err) throw err;
+// });
 
-connection.query = util.promisify(connection.query);
+// connection.query = util.promisify(connection.query);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db"
+  });
+}
 
+connection.connect();
 module.exports = connection;
