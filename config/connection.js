7c13451bc55b0b9fcd43c3d9fb1011ac3151c01a
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-31 12:18:00 -0400

    reset port connections

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 var connection;
-// const util = require("util");
+const util = require("util");
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Mr.Wills123!!",
-//   database: "burgers_db"
-// });
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Mr.Wills123!!",
+  database: "burgers_db"
+});
 
-// connection.connect(function (err) {
-//   if (err) throw err;
-// });
+connection.connect(function (err) {
+  if (err) throw err;
+});
 
-// connection.query = util.promisify(connection.query);
+connection.query = util.promisify(connection.query);
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
